f36b45afe367da359072519adb672d151b80e033
hb0313 <hbhandwaldar@gmail.com>
2022-08-09 14:38:22 -0400

    removed conflict

--- problem1/eval.py
+++
@@ -1,9 +0,0 @@
-## EVALUATION
-
-import sklearn
-import re
-sklearn.metrics.mutual_info_score(
-    news_topics['topic_num'].astype(int),
-    news_topics['topic_1_name'].apply(lambda x: int(re.findall("\d+", x)[0])).tolist()
-)
-
